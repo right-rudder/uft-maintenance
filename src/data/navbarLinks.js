71cfd0e309fb3a051be1b29a38c4000da869b8b6
Rodrigo J. R. Nunes <hannziegel@gmail.com>
2025-08-29 15:22:14 -0300

    Update Links

--- src/data/navbarLinks.js
+++ src/data/navbarLinks.js
@@ -1,55 +1,11 @@
 export const navbarLinks = [
   {
-    name: "Start Here",
-    link: "/pilot-training-guide",
-    submenu: [],
-  },
-  {
-    name: "Pilot Programs",
-    link: "/programs",
-    submenu: [
-      {
-        name: "Private Pilot",
-        link: "/programs/private-pilot-course",
-      },
-      {
-        name: "Instrument Rating",
-        link: "/programs/instrument-rating",
-      },
-      {
-        name: "Commercial Pilot",
-        link: "/programs/commercial-pilot-training",
-      },
-      {
-        name: "Multi-Engine Rating",
-        link: "/programs/multi-engine-rating",
-      },
-      {
-        name: "Flight Instructor",
-        link: "/programs/certified-flight-instructor-cfi",
-      },
-      // {
-      //   name: "Sport Pilot",
-      //   link: "/programs/sport-pilot",
-      // },
-    ],
-  },
-  {
-    name: "University Program",
-    link: "/university-program",
-    submenu: [],
-  },
-  {
     name: "Services",
     link: "/services/maintenance",
     submenu: [
       {
         name: "Maintenance",
         link: "/services/maintenance",
-      },
-      {
-        name: "CATS Testing",
-        link: "/services/cats",
       },
     ],
   },
@@ -61,27 +17,15 @@ export const navbarLinks = [
         name: "Our Crew",
         link: "/about/our-crew",
       },
-      {
-        name: "Our Fleet",
-        link: "/about/our-fleet",
-      },
-      {
-        name: "Our Simulator",
-        link: "/about/our-simulator",
-      },
-      {
-        name: "Financing",
-        link: "/financing",
-      },
-      {
-        name: "Blog",
-        link: "/blog",
-      },
+      // {
+      //   name: "Blog",
+      //   link: "/blog",
+      // },
     ],
   },
   {
-    name: "Enroll Today",
-    link: "/enroll-now",
+    name: "Contact Us",
+    link: "/contact",
     submenu: [],
   },
 ];
