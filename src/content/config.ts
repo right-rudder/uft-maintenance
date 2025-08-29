87b7b5855187abc4db3453c96d99e99868bf7601
Rodrigo J. R. Nunes <hannziegel@gmail.com>
2025-08-29 15:14:18 -0300

    Modify pilot training to aircraft maintenance locations

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,22 +1,22 @@
 import { defineCollection, z } from "astro:content";
 
-const blogCollection = defineCollection({
-  type: "content",
-  // Type-check frontmatter using a schema
-  schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    author: z.string(),
-    category: z.string(),
-    tags: z.array(z.string()),
-    keywords: z.string().optional(),
-    readingTime: z.number(),
-    // Transform string to Date object
-    pubDate: z.coerce.date(),
-    updatedDate: z.coerce.date().optional(),
-    heroImage: z.string().optional(),
-  }),
-});
+// const blogCollection = defineCollection({
+//   type: "content",
+//   // Type-check frontmatter using a schema
+//   schema: z.object({
+//     title: z.string(),
+//     description: z.string(),
+//     author: z.string(),
+//     category: z.string(),
+//     tags: z.array(z.string()),
+//     keywords: z.string().optional(),
+//     readingTime: z.number(),
+//     // Transform string to Date object
+//     pubDate: z.coerce.date(),
+//     updatedDate: z.coerce.date().optional(),
+//     heroImage: z.string().optional(),
+//   }),
+// });
 
 const locationCollection = defineCollection({
   type: "content",
@@ -34,6 +34,6 @@ const locationCollection = defineCollection({
 });
 
 export const collections = {
-  blog: blogCollection,
-  "pilot-training": locationCollection,
+  // blog: blogCollection,
+  "aircraft-maintenance": locationCollection,
 };
